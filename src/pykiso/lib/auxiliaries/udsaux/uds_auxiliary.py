88f7a556295dc5a159cdd4e5410501465ebc9a3c
Theo Clementz <external.Clementz.Theo@de.bosch.com>
2025-08-04 17:05:36 +0200

    Return original response even if empty in legacy API

--- src/pykiso/lib/auxiliaries/udsaux/uds_auxiliary.py
+++ src/pykiso/lib/auxiliaries/udsaux/uds_auxiliary.py
@@ -183,7 +183,7 @@ class UdsAuxiliary(UdsBaseAuxiliary):
             False
         """
         try:
-            return self.send_uds(msg_to_send, response_required) or True
+            True if (response := self.send_uds(msg_to_send, response_required)) is None else response
         except self.errors.ResponseNotReceivedError as exc:
             raise exc
         except Exception:
